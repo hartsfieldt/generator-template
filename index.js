68c2576b7c1a0586c566e381cd1dbcdbd8b276c3
Your Name <you@example.com>
2022-09-23 16:30:16 -0400

    created questions and linked modules and import

--- index.js
+++ index.js
@@ -1,13 +1,76 @@
-// TODO: Include packages needed for this application
+//Node Modules
+const inquirer = require("inquirer");
+const fs = require("fs");
+//Import
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//inquirer to generate questions
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Please, enter your GitHub Username",
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "Hello, what is the name of your project?",
+    },
+    {
+      type: "input",
+      name: "table of contents",
+      message: "",
+    },
+    {
+      type: "input",
+      name: "Section",
+      message: "Installation Requirements",
+    },
+    {
+      type: "input",
+      name: "Section",
+      message: "Useage",
+    },
+    {
+      type: "input",
+      name: "Section",
+      message: "Contributing Guidelines",
+    },
+    {
+      type: "input",
+      name: "Section",
+      message: "Test",
+    },
+    {
+      type: "input",
+      name: "Section",
+      message: "Questions",
+      //Push contact name, gihHub profile and ways to contact here.
+    },
+  ]);
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(fileName, data);
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+function init() {
+  promptUser().then((data) => {
+    console.log(data);
+    const pageMD = generateMarkdown(data);
+    writeToFile("./output/readme.md", pageMD);
+  });
+}
 
-// Function call to initialize app
 init();
+
+// TODO: Include packages needed for this application - Done
+// TODO: Create an array of questions for user input
+// TODO: Create a function to write README file
+// TODO: Create a function to initialize app
